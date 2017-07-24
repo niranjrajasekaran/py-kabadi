1766b2b5d0dec6fbb886abbcbb19e159e474f9a6
niranjfantain <niranj@fantain.com>
2017-07-24 12:49:48 +0530

    pykabadi as a package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = 'py-kabadi',
+    version = '1.0.3',
+    description = 'Kabadi Wrapper',
+    long_description = 'Kabadi Wrapper',
+
+    author = 'Niranj Rajasekaran',
+    author_email = 'niranj@fantain.com',
+    url = 'https://github.com/niranjfantain/py-kabadi',
+    package_dir={'': 'src'},
+    packages=[''],
+    install_requires=['certifi==2017.4.17', 'chardet==3.0.4', 'idna==2.5', 'pkg-resources==0.0.0',
+                      'requests==2.18.1', 'urllib3==1.21.1'],
+    entry_points='''
+        [console_scripts]
+        pykabadi=src.pykabadi:RcaApp
+    '''
+)
